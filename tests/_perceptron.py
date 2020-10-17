1a4cd7334311d5956c3510dc4f854b6cdfe17f71
Roman Zacharek <zacharekedward@gmail.com>
2020-10-17 22:18:32 +0200

    Tests for perceptron

--- tests/_perceptron.py
+++ tests/_perceptron.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import pandas as pd
 import sys 
 sys.path.append('..')
 from perceptron import Perceptron as Perceptronnn
@@ -24,8 +25,24 @@ class TestPerceptron(unittest.TestCase):
         self.assertListEqual(given_result, expected_result)
 
     def test_iris_data_set(self):
-        
+        iris_data = pd.read_csv("Iris.csv") 
+        iris_data = iris_data[:100] 
+        iris_data = iris_data.sample(frac=1)
+        x = iris_data[:100].to_numpy()
+        x = x[:,:5]
+        y = iris_data[:100].to_numpy()
+        y = y[:,5]
+        y = np.array([1 if val == 'Iris-setosa' else -1 for val in y] )
 
+        train_x = x[:80]
+        train_y = y[:80]
+
+        test_x = x[81:]
+        test_y = y[81:]
+
+        myperceptron =  Perceptronnn(train_x, train_y, 1)
+        myperceptron.train()
+        self.assertListEqual(myperceptron.predict(test_x).tolist(),test_y.tolist())
 
 if __name__ == '__main__':
     unittest.main()
