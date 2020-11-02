d4cf6d8a017832e1b3889af541f11614355769f9
Roman Zacharek <zacharekedward@gmail.com>
2020-11-02 10:05:42 +0100

    Modified perceptron to work better

--- perceptron.py
+++ perceptron.py
@@ -35,24 +35,27 @@ class Perceptron:
         else:
             return argv[1][index] * np.dot(w, argv[0][index])
 
+ 
+
     """Training the model"""
 
     def train(self):
         (number_of_rows, number_of_cols) = self.train_set_x.shape
-        self.w = np.zeros((1, number_of_cols))
-
-        while(True):
+        self.w = np.zeros((1, number_of_cols + 1))
+        iter = 0
+        while(iter < 10000):
             count_wrong_classification = 0
 
             for i in range(len(self.train_set_x)):
-
-                if self.sign_of_classification(i,self.w) <= 0:
-                    self.w = np.add(self.w,self.learning_rate*self.train_set_y[i]*self.train_set_x[i]) 
+                x_vector = self.train_set_x[i]
+                x_vector = np.concatenate(([1], x_vector))
+                if self.train_set_y[i] * np.dot(self.w, x_vector) <= 0:
+                    self.w = np.add(self.w,self.learning_rate*self.train_set_y[i]*x_vector) 
                     count_wrong_classification += 1
             
             if count_wrong_classification == 0: break
-        
-        return self
+            iter += 1
+        return self.w
 
     """Predicts the classification based on the input
 
@@ -65,14 +68,31 @@ class Perceptron:
         (number_of_rows, number_of_columns) = test_set_x.shape
         self.predicted_y = np.arange(number_of_rows)
         for i in range(number_of_rows):
-            if np.dot(self.w, test_set_x[i]) > 0:
+            x_vector =  test_set_x[i]
+            x_vector = np.concatenate(([1],x_vector))
+            if np.dot(self.w, x_vector) > 0:
                 self.predicted_y[i] = 1
             else:
                 self.predicted_y[i] = -1
         
         return np.array(self.predicted_y)
 
+    """Computes the prediction error
 
+    Attributes:
+        axis_x -- the x input of the data
+        axis_y -- the correct prediction
+    """
+
+    def prediction_error(self, axis_x, axis_y):
+        prediction_error = 0.0
+
+        predicted_y = self.predict(axis_x)
+
+        for i in range(len(predicted_y)):
+            if (predicted_y[i] != axis_y[i]): prediction_error += 1.0
+        
+        return prediction_error/len(predicted_y)
 
 
                 
