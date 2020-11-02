6f9e5c7ac6c836c2144cbdc5ad0122079749e382
Roman Zacharek <zacharekedward@gmail.com>
2020-11-02 10:06:45 +0100

    Adapted the tests to the new perceptron implementation, all work good

--- tests/_perceptron.py
+++ tests/_perceptron.py
@@ -3,17 +3,14 @@ import numpy as np
 import pandas as pd
 import sys 
 sys.path.append('..')
-from perceptron import Perceptron as Perceptronnn
-from sklearn.datasets import load_iris
-from sklearn.utils import check_random_state
-from sklearn.linear_model import Perceptron as Percy
+from perceptron import Perceptron
 
 
 class TestPerceptron(unittest.TestCase):
 
     def test_sign_of_classification(self):
         
-        myperceptron =  Perceptronnn(np.zeros((2,1)), np.zeros((2)), 1)
+        myperceptron =  Perceptron(np.zeros((2,1)), np.zeros((2)), 1)
 
         x_vector = [[1,1,1], [1,2,3], [-1,-1,-1]]
         w_vector = [1,1,1]
@@ -32,7 +29,7 @@ class TestPerceptron(unittest.TestCase):
         x = x[:,:5]
         y = iris_data[:100].to_numpy()
         y = y[:,5]
-        y = np.array([1 if val == 'Iris-setosa' else -1 for val in y] )
+        y = np.array([1 if val == 'Iris-setosa' else -1 for val in y])
 
         train_x = x[:80]
         train_y = y[:80]
@@ -40,9 +37,29 @@ class TestPerceptron(unittest.TestCase):
         test_x = x[81:]
         test_y = y[81:]
 
-        myperceptron =  Perceptronnn(train_x, train_y, 1)
+        myperceptron =  Perceptron(train_x, train_y, 1)
         myperceptron.train()
         self.assertListEqual(myperceptron.predict(test_x).tolist(),test_y.tolist())
+    
+    def test_prediction_error(self):
+        iris_data = pd.read_csv("Iris.csv") 
+        iris_data = iris_data[:100] 
+        iris_data = iris_data.sample(frac=1)
+        x = iris_data[:100].to_numpy()
+        x = x[:,:5]
+        y = iris_data[:100].to_numpy()
+        y = y[:,5]
+        y = np.array([1 if val == 'Iris-setosa' else -1 for val in y])
+
+        train_x = x[:80]
+        train_y = y[:80]
+
+        test_x = x[81:]
+        test_y = y[81:]
+
+        myperceptron =  Perceptron(train_x, train_y, 1)
+        myperceptron.train()
+        self.assertEqual(myperceptron.prediction_error(test_x, test_y), 0)
 
 if __name__ == '__main__':
     unittest.main()
